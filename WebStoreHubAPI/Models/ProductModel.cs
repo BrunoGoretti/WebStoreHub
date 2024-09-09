3a54c111aba7b6fddc3a487c59a4e5d4194fcd90
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-09-09 22:51:54 +0400

    ProductType

--- WebStoreHubAPI/Models/ProductModel.cs
+++ WebStoreHubAPI/Models/ProductModel.cs
@@ -11,5 +11,6 @@ namespace WebStoreHubAPI.Models
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public string ImageUrl { get; set; }
+        public string ProductType { get; set; }
     }
 }
