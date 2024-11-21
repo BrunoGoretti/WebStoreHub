51a8af3635ad651d5993810011c3d98aa68246af
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2024-11-21 17:51:01 +0400

    Add login button

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-
+import { LogInComponent } from './components/log-in/log-in.component'; // Import LogInComponent
 
 export const routes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: '', redirectTo: '/products', pathMatch: 'full' }
+  { path: 'login', component: LogInComponent }, // Add login route
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // Default route
 ];
--- angular-frontend/src/app/components/header/header.component.ts
+++ angular-frontend/src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [RouterModule], // Import RouterModule here
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
