269f31d531abe66f6898570a919d4454cd85a0a0
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2025-03-08 17:03:54 +0400

    Pagianton component

---
+++ angular-frontend/src/app/components/pagination/pagination.component.spec.ts
@@ -0,0 +1,14 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PaginationComponent } from './pagination.component';
+
+describe('PaginationComponent', () => {
+  let component: PaginationComponent;
+  let fixture: ComponentFixture<PaginationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PaginationComponent]
+    })
+    .compileComponents();
+  });
+});
--- angular-frontend/src/app/components/search-product/search-product.component.ts
+++ angular-frontend/src/app/components/search-product/search-product.component.ts
@@ -3,22 +3,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../../services/product/product.service';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
 
 @Component({
   selector: 'app-search-product',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './search-product.component.html',
-  styleUrl: './search-product.component.css'
+  styleUrls: ['./search-product.component.css', '../../components/pagination/pagination.component.css']
 })
-export class SearchProductComponent implements OnInit {
-  products: Product[] = [];
-  searchQuery: string = '';
-  paginatedProducts: Product[] = [];
-  currentPage: number = 1;
-  itemsPerPage: number = 15;
 
-  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {}
+export class SearchProductComponent extends PaginationComponent implements OnInit {
+  searchQuery: string = '';
+
+  constructor(private route: ActivatedRoute, private productService: ProductService, private router: Router) {
+    super();
+  }
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
@@ -37,72 +37,5 @@ export class SearchProductComponent implements OnInit {
 
   onProductClick(product: Product): void {
     this.router.navigate(['/product', product.productId]);
-  }
-
-  updatePaginatedProducts(): void {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    this.paginatedProducts = this.products.slice(startIndex, endIndex);
-  }
-
-  changePage(page: number | string): void {
-    if (typeof page === 'number') {
-      this.currentPage = page;
-      this.updatePaginatedProducts();
-    }
-  }
-
-  nextPage(): void {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      this.updatePaginatedProducts();
-    }
-  }
-
-  previousPage(): void {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      this.updatePaginatedProducts();
-    }
-  }
-
-  get totalPages(): number {
-    return Math.ceil(this.products.length / this.itemsPerPage);
-  }
-
-  get pages(): (number | string)[] {
-    const totalPages = this.totalPages;
-    const currentPage = this.currentPage;
-    const maxVisiblePages = 5;
-
-    if (totalPages <= maxVisiblePages) {
-
-      return Array.from({ length: totalPages }, (_, i) => i + 1);
-    } else {
-
-      const pages: (number | string)[] = [];
-      const halfVisible = Math.floor(maxVisiblePages / 2);
-
-      pages.push(1);
-
-      if (currentPage > halfVisible + 1) {
-        pages.push('...');
-      }
-
-      const start = Math.max(2, currentPage - halfVisible);
-      const end = Math.min(totalPages - 1, currentPage + halfVisible);
-
-      for (let i = start; i <= end; i++) {
-        pages.push(i);
-      }
-
-      if (currentPage < totalPages - halfVisible - 1) {
-        pages.push('...');
-      }
-
-      pages.push(totalPages);
-
-      return pages;
-    }
   }
 }
