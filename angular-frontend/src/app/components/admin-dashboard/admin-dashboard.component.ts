95d2527d1af8632ff3fdb05214569569141b1d82
Gor <104521859+BrunoGoretti@users.noreply.github.com>
2025-11-03 21:34:33 +0400

    add product in admin dashboard init

--- angular-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ angular-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -1,26 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css',
 })
 export class AdminDashboardComponent implements OnInit {
   userId: number | null = null;
+  showAddProductForm = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.authService.isLoggedIn().subscribe((islogged) => {
-      if (!islogged)
-      {
+      if (!islogged) {
         console.warn('User not logged in.');
         this.router.navigate(['/login']);
       }
     });
+  }
+
+  toggleAddProductForm() {
+    this.showAddProductForm = !this.showAddProductForm;
+  }
+
+  onSubmitAddProduct(event: Event) {
+    event.preventDefault();
+    this.toggleAddProductForm();
   }
 }
